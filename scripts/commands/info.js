ed99489edb7cece6bf3deeda21aa0a8d8fe98a15
Weird kenji <weridkenji@gmail.com>
2025-04-21 05:29:29 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -2,46 +2,32 @@ module.exports.config = {
     name: "admin",
     version: "1.0.0",
     permission: 0,
-    credits: "nayan",
+    credits: "Weird Kenji",
     prefix: true,
     description: "",
     category: "prefix",
     usages: "",
-    cooldowns: 5,
-    dependencies: 
-	{
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
+    cooldowns: 5
 };
-module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
-const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-  
-var callback = () => api.sendMessage({body:`
---------------------------------------------
-𝐍𝐚𝐦𝐞       : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
-𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐓𝐚𝐧𝐠𝐚𝐢𝐥, 𝐃𝐡𝐚𝐤𝐚
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐌𝐨𝐲𝐦𝐨𝐧𝐬𝐢𝐧𝐡, 𝐃𝐡𝐚𝐤𝐚 𝐁𝐲𝐩𝐚𝐬𝐬
-𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
-𝐀𝐠𝐞           : 𝟏𝟖+
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
-𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : mohammadnayan447@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801615298449
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/MOHAMMADNAYAN
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/www.xnxx.com169`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
-    fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
-fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
-   };
+
+module.exports.run = async function({ api, event }) {
+    const moment = require("moment-timezone");
+    const timeNow = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+
+    return api.sendMessage(`
+╭───〔 ⚡ About Me 〕───╮
+│
+│ 📝 Name           : 𝐖𝐞𝐢𝐫𝐝 𝐊𝐞𝐧𝐣𝐢
+│ 📅 Age            : 𝟏𝟖+
+│ 🎂 Date of Birth  : 𝟎𝟒-𝟎𝟔
+│ ☪️ Religion       : 𝐈𝐬𝐥𝐚𝐦
+│ 🚹 Gender         : 𝐌𝐚𝐥𝐞
+│ 🎓 Work           : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
+│ 💔 Relationship   : 𝐒𝐢𝐧𝐠𝐥𝐞
+│ 🏠 Address        : 𝐃𝐡𝐚𝐤𝐚, 𝐆𝐚𝐳𝐢𝐩𝐮𝐫
+│ 🎨 Fav Color      : 𝐁𝐥𝐮𝐞
+│ 🌐 Facebook Link  : fb.com/profile.php?id=61572891756852
+│
+╰──〔 ⏰ Time: ${timeNow} 〕──╯
+`, event.threadID);
+};
